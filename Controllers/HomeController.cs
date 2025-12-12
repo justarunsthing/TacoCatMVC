d7de8f202899b60a83105bb83dab16ddcab9192e
justarunsthing <justarunsthing@outlook.com>
2025-12-12 20:57:39 +0000

    Removed Privacy action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,11 +11,6 @@ namespace TacoCatMVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
