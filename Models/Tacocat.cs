b34802e37c8d08db6498934f1bcc2650f49fa339
justarunsthing <justarunsthing@outlook.com>
2025-12-14 18:02:56 +0000

    Added Tacocat model

---
+++ Models/Tacocat.cs
@@ -0,0 +1,9 @@
+﻿namespace TacoCatMVC.Models
+{
+    public class Tacocat
+    {
+        public string? Input { get; set; }
+        public string? Result { get; set; }
+        public bool IsPalindrome { get; set; }
+    }
+}
