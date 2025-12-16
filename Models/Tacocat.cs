45332c07230b958766a2c9e98511eabea8523edb
justarunsthing <justarunsthing@outlook.com>
2025-12-16 08:13:24 +0000

    Added Message property

--- Models/Tacocat.cs
+++ Models/Tacocat.cs
@@ -5,5 +5,6 @@
         public string? Input { get; set; }
         public string? Result { get; set; }
         public bool IsPalindrome { get; set; }
+        public string? Message { get; set; }
     }
 }
